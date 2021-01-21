7f1626773c7f740610a2240c8893613156c9e4cc
Emsah04 <Emsah04@users.noreply.github.com>
2021-01-21 15:27:58 +0100

    Update List.py

--- List.py
+++ List.py
@@ -1,17 +1,17 @@
-rad = int(input("Antal rader? "))
-kol = int(input("Antal kolumner? "))
-d1=[]
-for i in range(rad):
-    d1.append([])
+x = int(input("Antal rader? "))
+y = int(input("Antal kolumner? "))
+cord=[]
+for i in range(x):
+    cord.append([])
     print("vad ska rad",i+1,"vara. Skriv med mellanslag")
     ap = input()
     split = ap.split()
     ma = map(int, split)
     d2=list(ma)
-    for l in range(kol):
-        d1[i].append([])
-        d1[i][l].append(d2[l])
-        d1[i][l].append(0)
-        d1[i][l].append(0)
-for z in range(len(d1)):
-    print(d1[z])
+    for l in range(y):
+        cord[i].append([])
+        cord[i][l].append(d2[l])
+        cord[i][l].append(0)
+        cord[i][l].append(0)
+for z in range(len(cord)):
+    print(cord[z])
