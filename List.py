80153d9d87eb4a0ce694243b6f5a2ec3758c5e3b
Emsah04 <Emsah04@users.noreply.github.com>
2021-01-21 15:22:43 +0100

    Update List.py

--- List.py
+++ List.py
@@ -9,6 +9,9 @@ for i in range(rad):
     ma = map(int, split)
     d2=list(ma)
     for l in range(kol):
-        d1[i].append(d2[l])
+        d1[i].append([])
+        d1[i][l].append(d2[l])
+        d1[i][l].append(0)
+        d1[i][l].append(0)
 for z in range(len(d1)):
     print(d1[z])
