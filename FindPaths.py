6255cc801343cf03732bf8e54e9b25a9825df66e
Linus Zetterman <linuszetterman123@gmail.com>
2021-01-25 16:12:21 +0100

    Added recursvive funtion that looks around

--- FindPaths.py
+++ FindPaths.py
@@ -1,5 +1,26 @@
-def lookAround(currentPos): #sparar paths
-    print(currentPos)
+def lookAround(y, x, i, lista, coords): #sparar paths        
+    lista.append(coords[x][y])
+
+    if i == 1:
+        return lookAround(x, y + 1, i + 1, lista, coords)
+    elif i == 2:
+        return lookAround(x + 1, y, i + 1, lista, coords)
+    elif i == 3:
+        return lookAround(x, y - 1, i + 1, lista, coords)
+    elif i == 4:
+        return lookAround(x, y - 1, i + 1, lista, coords)
+    elif i == 5:
+        return lookAround(x - 1, y, i + 1, lista, coords)
+    elif i == 6:
+        return lookAround(x - 1, y, i + 1, lista, coords)
+    elif i == 7:
+        return lookAround(x, y + 1, i + 1, lista, coords)
+    elif i == 8:
+        return lookAround(x, y + 1, i + 1, lista, coords)
+    else:
+        return lista
+
+
 
 def findPaths():
     lookAround()
